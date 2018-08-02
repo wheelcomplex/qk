hackcd/quic-go

pass the whole packet (including the header) to the session

client_test.go
@@ -518,11 +518,6 @@ var _ = Describe("Client", func() {
 	})
 
 	It("cuts packets at the payload length", func() {
-		sess := NewMockPacketHandler(mockCtrl)
-		sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
-			Expect(packet.data).To(HaveLen(123))
-		})
-		cl.session = sess
 		b := &bytes.Buffer{}
 		hdr := &wire.Header{
 			IsLongHeader:     true,
@@ -533,6 +528,11 @@ var _ = Describe("Client", func() {
 			Version:          versionIETFFrames,
 		}
 		Expect(hdr.Write(b, protocol.PerspectiveClient, versionIETFFrames)).To(Succeed())
+		sess := NewMockPacketHandler(mockCtrl)
+		sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
+			Expect(packet.data).To(HaveLen(123 + b.Len()))
+		})
+		cl.session = sess
 		err := cl.handlePacket(addr, append(b.Bytes(), make([]byte, 456)...))
 		Expect(err).ToNot(HaveOccurred())
 	})

server_test.go
@@ -319,12 +319,6 @@ var _ = Describe("Server", func() {
 		})
 
 		It("cuts packets at the payload length", func() {
-			sess := NewMockPacketHandler(mockCtrl)
-			sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
-				Expect(packet.data).To(HaveLen(123))
-			})
-
-			serv.supportsTLS = true
 			b := &bytes.Buffer{}
 			hdr := &wire.Header{
 				IsLongHeader:     true,
@@ -335,6 +329,12 @@ var _ = Describe("Server", func() {
 				Version:          versionIETFFrames,
 			}
 			Expect(hdr.Write(b, protocol.PerspectiveClient, versionIETFFrames)).To(Succeed())
+			sess := NewMockPacketHandler(mockCtrl)
+			sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
+				Expect(packet.data).To(HaveLen(123 + b.Len()))
+			})
+
+			serv.supportsTLS = true
 			sessionHandler.EXPECT().Get(connID).Return(sess, true)
 			err := serv.handlePacket(nil, append(b.Bytes(), make([]byte, 456)...))
 			Expect(err).ToNot(HaveOccurred())

wheelcomplex/qk

client_test.go
@@ -639,11 +639,7 @@ var _ = Describe("Client", func() {
 	})
 
 	It("cuts packets at the payload length", func() {
-		sess := NewMockQuicSession(mockCtrl)
-		sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
-			Expect(packet.data).To(HaveLen(123))
-		})
-		cl.session = sess
+		b := &bytes.Buffer{}
 		hdr := &wire.Header{
 			IsLongHeader:     true,
 			Type:             protocol.PacketTypeHandshake,
@@ -653,10 +649,17 @@ var _ = Describe("Client", func() {
 			PacketNumberLen:  protocol.PacketNumberLen1,
 			Version:          versionIETFFrames,
 		}
+		Expect(hdr.Write(b, protocol.PerspectiveClient, versionIETFFrames)).To(Succeed())
+		hdr.Raw = b.Bytes()
+		sess := NewMockQuicSession(mockCtrl)
+		sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
+			Expect(packet.data).To(HaveLen(123 + b.Len()))
+		})
+		cl.session = sess
 		err := cl.handlePacketImpl(&receivedPacket{
 			remoteAddr: addr,
 			header:     hdr,
-			data:       make([]byte, 456),
+			data:       append(b.Bytes(), make([]byte, 456)...),
 		})
 		Expect(err).ToNot(HaveOccurred())
 	})

server_test.go
@@ -335,14 +335,6 @@ var _ = Describe("Server", func() {
 		})
 
 		It("cuts packets at the payload length", func() {
-			sess := NewMockQuicSession(mockCtrl)
-			sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
-				Expect(packet.data).To(HaveLen(123))
-			})
-			sess.EXPECT().GetVersion().Return(protocol.VersionTLS)
-			sessionHandler.EXPECT().Get(connID).Return(sess, true)
-
-			serv.supportsTLS = true
 			b := &bytes.Buffer{}
 			hdr := &wire.Header{
 				IsLongHeader:     true,
@@ -354,6 +346,15 @@ var _ = Describe("Server", func() {
 				Version:          versionIETFFrames,
 			}
 			Expect(hdr.Write(b, protocol.PerspectiveClient, versionIETFFrames)).To(Succeed())
+			hdr.Raw = b.Bytes()
+			sess := NewMockQuicSession(mockCtrl)
+			sess.EXPECT().GetVersion().Return(versionIETFFrames)
+			sess.EXPECT().handlePacket(gomock.Any()).Do(func(packet *receivedPacket) {
+				Expect(packet.data).To(HaveLen(123 + b.Len()))
+			})
+
+			serv.supportsTLS = true
+			sessionHandler.EXPECT().Get(connID).Return(sess, true)
 			err := serv.handlePacket(nil, append(b.Bytes(), make([]byte, 456)...))
 			Expect(err).ToNot(HaveOccurred())
 		})